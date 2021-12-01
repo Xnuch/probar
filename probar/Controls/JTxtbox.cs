f0e856584f2727f72a6b8ac9a9e7e7d11956cad9
Ulises Saenz <xnuch5@gmail.com>
2021-11-30 18:01:51 -0700

    Agregar archivos de proyecto.

---
+++ probar/Controls/JTxtbox.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace probar.Controls
+{
+    public partial class JTxtbox : UserControl
+    {
+        //Fields
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlineStyle = false;
+
+        //Constructor
+        public JTxtbox()
+        {
+            InitializeComponent();
+        }
+
+        //propiedades
+        public Color BorderColor
+        {
+            get
+            {
+                return BorderColor;
+            }
+
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+        public int BorderSize
+        {
+            get
+            {
+                return BorderSize;
+            }
+
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        public bool Underlinestyle
+        {
+            get
+            {
+                return Underlinestyle;
+            }
+
+            set
+            {
+                underlineStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        //Metodo Override
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            //Dibujar borde
+            using (Pen penBorder=new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+                if (underlineStyle) //Estilo line
+                    graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                else //Estilo normal
+                    graph.DrawLine(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if(this.DesignMode)
+                UpdateControlHeight();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        //Metodo privado
+        private void UpdateControlHeight()
+        {
+            if (textBox1.Multiline == false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize = new Size(0, txtHeight);
+                textBox1.Multiline = false;
+
+                this.Height = textBox1.Height + this.Padding.Top + this.Padding.Bottom;
+            }
+        }
+    }
+}
